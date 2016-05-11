69bb04c36d66478e46adf9b3880c56e050062517
Sanches <Sanches@10.13.114.126>
2016-05-10 21:05:50 -0300

    adicionado metodoadiciona produto

---
+++ src/RegradeNegocio/Produtos.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RegradeNegocio;
+
+/**
+ *
+ * @author Sanches
+ */
+public class Produtos {
+
+    private int qtdEstoque;
+    private double precoProduto;
+    private int idProduto;
+    private String tipoDeProduto;
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getTipoDeProduto() {
+        return tipoDeProduto;
+    }
+
+    public void setTipoDeProduto(String tipoDeProduto) {
+        this.tipoDeProduto = tipoDeProduto;
+    }
+
+    public int getQtdEstoque() {
+        return qtdEstoque;
+    }
+
+    public void setQtdEstoque(int qtdEstoque) {
+        this.qtdEstoque = qtdEstoque;
+    }
+
+    public double getPrecoProduto() {
+        return precoProduto;
+    }
+
+    public void setPrecoProduto(double precoProduto) {
+        this.precoProduto = precoProduto;
+    }
+
+    public void adicionarProduto(String tipoDeProduto, int idProduto, int qtdEstoque, double precoProduto) {
+
+        this.idProduto = idProduto;
+        this.tipoDeProduto = tipoDeProduto;
+        this.qtdEstoque = qtdEstoque;
+        this.precoProduto = precoProduto;
+
+    }
+
+    public void deletarProduto(String idProduto) {
+
+    }
+
+}
--- src/RegradeNegocio/teste.java
+++ src/RegradeNegocio/teste.java
@@ -12,13 +12,21 @@ package RegradeNegocio;
 public class teste {
 
     public static void main(String[] args) {
-       Atendimento a = new Atendimento();
-
-       a.cadastroCliente("Daniel", "405.990.188.10", "sanches_dado@hotmail.com", "968738481",
+        Atendimento a = new Atendimento();
+        Produtos p = new Produtos();
+        a.cadastroCliente("Daniel", "405.990.188.10", "sanches_dado@hotmail.com", "968738481",
                 "4437-2412", "483510336", "São Bernardo", "Rua Tiradentes", "sta Terezinha",
                 "bloco 11 apto 155", "09781220", "SP");
-        
-        System.out.print(a);
+
+        System.out.println(a);
+
+        String[] teste = new String[4];
+        int i = 0;
+        p.adicionarProduto("suplemento", 12345, 10, 1000);
+        System.out.println(p.getTipoDeProduto());
+        System.out.println(p.getIdProduto());
+        System.out.println(p.getQtdEstoque());
+        System.out.println(p.getPrecoProduto());
 
     }
 }
